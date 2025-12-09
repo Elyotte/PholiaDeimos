ca38f09904cf492956902395e41edad5a8e6aba5
Eliott <tan.eliott04@gmail.com>
2025-12-09 08:20:17 +0100

    adding default parameters

--- Scripts/Core/PatternBasedEnemy.cs
+++ Scripts/Core/PatternBasedEnemy.cs
@@ -3,7 +3,7 @@ using System;
 
 public class PatternBasedEnemy : Enemy
 {
-    protected void HorizontalSine(float delta, float sinusSpeed = 10, float moveOffset = 10)
+    protected void HorizontalSine(float delta, float sinusSpeed = 3.4f, float moveOffset = 200f)
     {
         float timePassed = (float)(0.000001 * Time.GetTicksUsec());
         GlobalPosition += new Vector2(Mathf.Sin(timePassed * sinusSpeed) * moveOffset, m_MoveSpeed) * delta;
