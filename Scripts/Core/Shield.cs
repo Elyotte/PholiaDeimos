2112ae3937c937abad559a77d65a10b113fe7343
Eliott <tan.eliott04@gmail.com>
2025-12-12 09:02:05 +0100

    adding graphisms and sign to shield enemies

--- Scripts/Core/Shield.cs
+++ Scripts/Core/Shield.cs
@@ -3,9 +3,34 @@ using System;
 
 public class Shield : Area2D
 {
+    CollisionShape2D shape;
+    NodePath path = "CollisionShape2D";
+
     public override void _Ready()
     {
+        shape = GetNode<CollisionShape2D>(path);
         Connect(SignalNames.AREA_ENTERED, this, nameof(OnAreaEntered));
+        CallDeferred(nameof(Start));
+    }
+
+    void Start() {
+        if(GameManager.deimos != null)
+        {
+            GameManager.deimos.onSplit += OnSplit;
+            GameManager.deimos.onResplit += OnResplit;
+        }
+    }
+
+    void OnSplit()
+    {
+        shape.Disabled = true;
+        Visible = false;
+    }
+
+    void OnResplit()
+    {
+        shape.Disabled = false;
+        Visible = true;
     }
 
     void OnAreaEntered(Area2D area)
@@ -18,6 +43,11 @@ public class Shield : Area2D
 
     public override void _ExitTree()
     {
+        if (GameManager.Instance != null)
+        {
+            GameManager.deimos.onSplit -= OnSplit;
+            GameManager.deimos.onResplit -= OnResplit;
+        }
         Disconnect(SignalNames.AREA_ENTERED,this, nameof(OnAreaEntered));
         base._ExitTree();
     }
